428b37010177f5330bc94669f12de31eb22b7af0
Mica56 <tayotomichael18@gmail.com>
2022-01-15 20:54:41 +0800

    Navigation bar working

---
+++ frontend/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+using student_concern_ticketing_system.frontend.Views;
+
+namespace student_concern_ticketing_system.frontend.ViewModels
+{
+    class HomeViewModel : ViewModelBase
+    {
+        public string Description { get; }
+    }
+}
