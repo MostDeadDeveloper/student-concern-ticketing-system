4dd54d7cb520d7ffca792d62008e142e7e20d3d0
Mica56 <tayotomichael18@gmail.com>
2022-02-02 13:38:21 +0800

    Some changes

--- frontend/ViewModels/LoginViewModel.cs
+++ frontend/ViewModels/LoginViewModel.cs
@@ -5,8 +5,4 @@ namespace student_concern_ticketing_system.frontend.ViewModels
         public string? txtEmail { get; set; }
         public string? txtPassword { get; set; }
     }
-<<<<<<< HEAD
-}
-=======
 } 
->>>>>>> 072feb764d595ec248954855b6b40870cbd2511a
