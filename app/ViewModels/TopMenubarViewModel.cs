7244b67b361de5dac8e0d3dcdadf8119bd872b8f
Mica56 <tayotomichael18@gmail.com>
2022-02-26 01:18:53 +0800

    Added TicketView components, some changes

--- app/ViewModels/TopMenubarViewModel.cs
+++ app/ViewModels/TopMenubarViewModel.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using app.Models;
+using ReactiveUI;
 
 namespace app.ViewModels
 {
     class TopMenubarViewModel : ViewModelBase
     {
-
+        
     }
 }
--- app/Views/TopMenuBar.axaml.cs
+++ app/Views/TopMenuBar.axaml.cs
@@ -3,11 +3,14 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
+using Avalonia.Controls.ApplicationLifetimes;
+using System;
 
 namespace app.Views
 {
     public partial class TopMenubar : UserControl
     {
+        private Button _ProfileBtn;
         public TopMenubar()
         {
             InitializeComponent();
@@ -16,6 +19,11 @@ namespace app.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+            var _ProfileDDMenu = this.Find<ComboBox>("ProfileDDMenu");
+            _ProfileBtn = this.Find<Button>("ProfileBtn");
+            if(_ProfileDDMenu.SelectedIndex == 2){
+                //DataContext = new LoginView();
+            }
         }
     }
 }
