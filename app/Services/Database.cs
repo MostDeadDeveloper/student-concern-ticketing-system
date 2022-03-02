d12b47ed8b7ea8774f59059aadb5534b7b0b4803
RandomUser <>
2022-03-03 01:34:31 +0800

    Added Database Configs and Rules

--- app/Services/Database.cs
+++ app/Services/Database.cs
@@ -1,16 +1,29 @@
 using System.Collections.Generic;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
 using app.Models;
 
 namespace app.Services
 {
-    public class Database
+    public class Database : DbContext
     {
-        public IEnumerable<Ticket> GetItems() => new[]
+        public DbSet<Ticket> Tickets { get; set; }
+
+        public string DbPath { get; }
+
+        public Database()
         {
-            new Ticket { TicketName = "First Ticket" },
-            new Ticket { TicketName = "Second Ticket" },
-            new Ticket { TicketName = "Third Ticket", IsFinished = true },
-        };
-        
+            DbPath = "blogging.db";
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+
+        public IEnumerable<Ticket> GetItems() => this.Tickets;
     }
 }
+
+
